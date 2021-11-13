a2dad610a05ea20a485be0810e088bd2f6d78737
St Germain <geoffrey40360@gmail.com>
2021-11-13 13:22:46 +0100

    git init

---
+++ eBayMIAGE/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  eBayMIAGE
+//
+//  Created by SAINT GERMAIN on 12/11/2021.
+//
+
+import SwiftUI
+ 
+
+private var url = "https://miagecarnet.azurewebsites.net/MIAGE_CLIENT"
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ eBayMIAGE/eBayMIAGEApp.swift
@@ -0,0 +1,17 @@
+//
+//  eBayMIAGEApp.swift
+//  eBayMIAGE
+//
+//  Created by SAINT GERMAIN on 12/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct eBayMIAGEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
